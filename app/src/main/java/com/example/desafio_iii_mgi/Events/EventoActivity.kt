69b20489db68d384bab621dd9cabae39d3a823ac
mariogmez <mariogomex199921@gmail.com>
2022-05-16 17:57:46 +0200

    CMT 17:57 16/05 DEV

--- app/src/main/java/com/example/desafio_iii_mgi/Events/EventoActivity.kt
+++ app/src/main/java/com/example/desafio_iii_mgi/Events/EventoActivity.kt
@@ -2,6 +2,7 @@ package com.example.desafio_iii_mgi.Events
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.net.http.SslCertificate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -16,10 +17,12 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.type.LatLng
 import kotlinx.android.synthetic.main.activity_evento.*
 import kotlinx.android.synthetic.main.activity_ficha_usuario.*
+import java.lang.StringBuilder
 
 class EventoActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
     private val db = FirebaseFirestore.getInstance()
     private lateinit var map:GoogleMap
+    private lateinit var even:Evento
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +40,7 @@ class EventoActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapL
                 txtNombreEve.text = it.get("nombre") as String?
                 txtFechaEve.text = it.get("fecha") as String?
                 txtHoraEve.text = it.get("hora") as String?
+
 
                 if ((it.get("lat") as Double) != 0.0 && (it.get("lon") as Double) != 0.0){
                     createMarker(com.google.android.gms.maps.model.LatLng(it.get("lat") as Double, it.get("lon") as Double))
@@ -58,28 +62,62 @@ class EventoActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapL
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
         map.setOnMapLongClickListener(this)
     }
 
     private fun createMarker(coor: com.google.android.gms.maps.model.LatLng) {
         val marker: MarkerOptions = MarkerOptions().position(coor)
         map.addMarker(marker)
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coor, 18f), 4000,null)
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(coor, 16f), 3000,null)
     }
 
     override fun onMapLongClick(coor: com.google.android.gms.maps.model.LatLng) {
+
+        val objIntent: Intent = intent
+        var ide: String? = objIntent.getStringExtra("id")
+
+        if (ide != null) {
+            db.collection("eventos").document(ide).get().addOnSuccessListener {
+                var nombre: String? = it.get("nombre") as String?
+                var fecha: String? = it.get("fecha") as String?
+                var hora: String? = it.get("hora") as String?
+                var lat: Double = it.get("lat") as Double
+                var lon :Double = it.get("lon") as Double
+                var event = Evento(ide, nombre.toString(),fecha.toString(),hora.toString(),lat,lon)
+                even = (event)
+            }
+        }
+
+
+
         createMarker(coor)
 
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Alerta")
         builder.setMessage("¿Esta seguro de establecer esta ubicacion para el evento?")
         builder.setPositiveButton("Si",{ dialogInterface: DialogInterface, i: Int ->
-            Toast.makeText(this, "si", Toast.LENGTH_SHORT).show()
+
+            if (ide != null) {
+                db.collection("eventos").document(ide).set(
+                    hashMapOf(
+                        "nombre" to even.nombre,
+                        "fecha" to even.fecha,
+                        "hora" to even.hora,
+                        "lat" to coor.latitude,
+                        "lon" to coor.longitude
+                    )
+                )
+            }
+
+            finish()
+            startActivity(getIntent())
 
         })
 
         builder.setNegativeButton("No",{ dialogInterface: DialogInterface, i: Int ->
-            Toast.makeText(this, "no", Toast.LENGTH_SHORT).show()
+            finish()
+            startActivity(getIntent())
         })
         builder.show()
     }
