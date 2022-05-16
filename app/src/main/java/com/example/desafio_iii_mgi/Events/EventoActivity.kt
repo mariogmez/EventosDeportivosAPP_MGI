a2c39a5b0321a156985787f23635a8989d3d617f
mariogmez <mariogomex199921@gmail.com>
2022-05-16 10:30:53 +0200

    CMT 10:30 16/05 DEV

--- app/src/main/java/com/example/desafio_iii_mgi/Events/EventoActivity.kt
+++ app/src/main/java/com/example/desafio_iii_mgi/Events/EventoActivity.kt
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.desafio_iii_mgi.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.type.LatLng
 import kotlinx.android.synthetic.main.activity_evento.*
 import kotlinx.android.synthetic.main.activity_ficha_usuario.*
 
-class EventoActivity : AppCompatActivity() {
+class EventoActivity : AppCompatActivity(), OnMapReadyCallback {
     private val db = FirebaseFirestore.getInstance()
+    private lateinit var map:GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_evento)
@@ -18,7 +24,6 @@ class EventoActivity : AppCompatActivity() {
         // RECUPERO EL DATOS DEL ID
         val objIntent: Intent = intent
         var id: String? = objIntent.getStringExtra("id")
-        Toast.makeText(this, id, Toast.LENGTH_SHORT).show()
 
         if (id != null) {
             db.collection("eventos").document(id).get().addOnSuccessListener {
@@ -29,6 +34,22 @@ class EventoActivity : AppCompatActivity() {
             }
         }
 
+        createFragment()
 
+
+    }
+
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        //val coordinates = LatLng()
     }
 }
