7161c7336686dfc577fd1c1405adbba752ee27ef
mariogmez <mariogomex199921@gmail.com>
2022-06-01 19:04:46 +0200

    CMT 19:04 01/06 DEV

--- app/src/main/java/com/example/desafio_iii_mgi/Admin/LIstadoEventosActivity.kt
+++ app/src/main/java/com/example/desafio_iii_mgi/Admin/LIstadoEventosActivity.kt
@@ -3,7 +3,6 @@ package com.example.desafio_iii_mgi.Admin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.KeyEvent
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
